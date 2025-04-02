f5684de67733b87a0a3b6576d3955a4c571de1ba
Zhen-Kai Gao <zkgao@asiaa.sinica.edu.tw>
2025-04-03 00:12:10 +0800

    Set top level folder for windows

--- carta_backend/settings/settings.py
+++ carta_backend/settings/settings.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import sys
 from pathlib import Path
 
@@ -15,14 +16,18 @@ class ProgramSettings:
             starting_folder: str = ".",
             version: str = "0.0.1",
     ) -> None:
-        self.host: str | None = host
-        self.socket_port: int = socket_port
-        self.frontend_folder: str = frontend_folder
-        self.backend_path: str = backend_path
-        self.user_folder_prefix: str = user_folder_prefix
-        self.top_level_folder: str = top_level_folder
-        self.starting_folder: str = os.path.abspath(starting_folder)
-        self.version: str = version
+        self.host = host
+        self.socket_port = socket_port
+        self.frontend_folder = frontend_folder
+        self.backend_path = backend_path
+        self.user_folder_prefix = user_folder_prefix
+        if platform.system() == "Windows":
+            cwd = Path.cwd()
+            self.top_level_folder = Path(f"{cwd.drive}\\")
+        else:
+            self.top_level_folder = top_level_folder
+        self.starting_folder = os.path.abspath(starting_folder)
+        self.version = version
 
         # Set host
         if self.host is None or self.host == "0.0.0.0":
